e3cb01d5133ed45977e8cd013727c80f4e4c9037
sam2829 <samscarisbrick@outlook.com>
2025-10-17 17:13:28 +0100

    Add components and styles folders. Add app.module.css file for base projects styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import "./App.css";
+// import "./App.css";
+import styles from "./App.module.css"
 
 function App() {
   return (
     <>
-      <div>
+      <div className={styles.App}>
         <p>Hugh's big important website...... tada!</p>
       </div>
     </>
