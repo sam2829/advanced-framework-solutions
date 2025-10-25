7c2be86eae693be882bf4c7c03a87c4e5e90f56e
sam2829 <samscarisbrick@outlook.com>
2025-10-25 13:28:30 +0100

    Remove col import from homepage as not used

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -1,4 +1,4 @@
-import { Col, Container, Row } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import styles from "../../styles/Homepage.module.css";
 import HomepageHeroImage from "./HomepageHeroImage";
 import HomepageServices from "./HomepageServices";
