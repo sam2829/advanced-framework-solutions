719a891337a8ecf7daace34289f1a89987cc2c57
sam2829 <samscarisbrick@outlook.com>
2025-10-25 10:49:57 +0100

    Add servic subheading and text for homepage

--- src/pages/homepage/Homepage.tsx
+++ src/pages/homepage/Homepage.tsx
@@ -11,15 +11,15 @@ const Homepage = () => {
       {/* main section */}
       <main>
         <Container>
-          <h2>Services</h2>
-          <p>
+          <h2 className={styles.SubHeading}>Services</h2>
+          <p className={styles.Text}>
             With years of experience in concrete and structural framework, we
             provide safe, efficient, and cost-effective solutions for buildings,
             bridges, and large-scale infastructure projects. We work closely
             with architects, contractors, and clients to ensure each design is
             delivered with precision and reliability.
           </p>
-          <p>
+          <p className={styles.Text}>
             My Services include full structural formwork design, load and stress
             calculations, and project consulting tailored to your specific
             needs.
