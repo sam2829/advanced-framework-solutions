254361eadb6ea899702ef3deb38601dd5801b370
sam2829 <samscarisbrick@outlook.com>
2025-11-14 12:43:44 +0000

    Remove basename which was added for github pages

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter basename="/advanced-framework-solutions">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </StrictMode>
